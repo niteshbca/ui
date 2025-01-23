8e26a8e5691ec56741667924da6e63eef2ee1ecd
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2025-01-23 10:27:16 +0530

    Update DeliveryPage.js

--- frontend/src/components/DeliveryPage.js
+++ frontend/src/components/DeliveryPage.js
@@ -19,7 +19,7 @@ const DeliveryPage = () => {
   const handleAddClick = async () => {
     try {
       console.log('Sending request to backend with input:', inputValue, 'and godown:', godown.name);
-      const response = await axios.post('http://15.156.6.220:5000/api/checkAndAddItem', {
+      const response = await axios.post('http://13.233.98.76:5000/api/checkAndAddItem', {
         input: inputValue,
         godownName: godown.name, // Include godown name in the request body
       });
